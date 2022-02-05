083344845346dee76c4f004702794023f5fd4722
Jaeeun Yang <yangtkboy@gmail.com>
2022-02-05 22:07:52 +0900

    add Auto Trading Code

---
+++ AutoConnect.py
@@ -0,0 +1,18 @@
+from pywinauto import application
+import time
+import os
+
+key_file_path = 'C:\\Users\\yangt\\key\\creonplus'
+start_command = open(key_file_path).read()
+
+os.system('taskkill /IM coStarter* /F /T')
+os.system('taskkill /IM CpStart* /F /T')
+os.system('taskkill /IM DibServer* /F /T')
+os.system('wmic process where "name like \'%coStarter%\'" call terminate')
+os.system('wmic process where "name like \'%CpStart%\'" call terminate')
+os.system('wmic process where "name like \'%DibServer%\'" call terminate')
+time.sleep(5)        
+
+app = application.Application()
+app.start(start_command)
+time.sleep(60)
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AutoTrading
+
+
+## 참고 자료
+
+[파이썬 주식 투자 자동화 강의](https://www.youtube.com/watch?v=4DzGOpsT3bw&list=PLU9-uwewPMe0fB60VIMuKFV7gPDXmyOzp&index=2&ab_channel=%EC%A1%B0%EC%BD%94%EB%94%A9JoCoding)
+
+[Daum 금융](https://finance.daum.net/)
+
+[파이썬 증권 데이터 분석 github](https://github.com/INVESTAR/StockAnalysisInPython)
+-> ch08 03 EtfAlgoTrader.py
